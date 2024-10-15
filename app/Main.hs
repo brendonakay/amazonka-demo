a9ef79f2f948a3f18999e1e5ee22dcaf50800eba
Brendon A. Kay <bak04280@gmail.com>
2024-10-14 19:24:54 -0600

    List, Put, and Describe table functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,42 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
--- {-# LANGUAGE RecordWildCards #-}
-
 module Main (main) where
 
 import qualified Amazonka as AWS
 import Amazonka.DynamoDB
-  ( BillingMode (BillingMode_PAY_PER_REQUEST),
-    KeyType (KeyType_HASH),
+  ( AttributeValue (S),
+    BillingMode (BillingMode_PAY_PER_REQUEST),
+    KeyType (KeyType_HASH, KeyType_RANGE),
     ScalarAttributeType (ScalarAttributeType_S),
     newAttributeDefinition,
     newCreateTable,
+    newDeleteTable,
+    newDescribeTable,
     newKeySchemaElement,
+    newPutItem,
   )
 import Amazonka.DynamoDB.Lens
   ( createTableResponse_tableDescription,
     createTable_attributeDefinitions,
     createTable_billingMode,
+    deleteTableResponse_tableDescription,
+    describeTableResponse_table,
+    putItemResponse_attributes,
+    putItem_item,
   )
 import Amazonka.DynamoDB.ListTables
 import Amazonka.S3.ListBuckets
--- Amazonka's core functions
-
-import Control.Lens -- For using lenses
+import Control.Lens
+import qualified Data.HashMap.Strict as HM
 import Data.List.NonEmpty
+import Data.Text (Text)
 import qualified System.IO as IO
+
+-- Amazonka's core functions
+-- TODO:
+--  - Pretty print output from IO functions. They usually return JSON.
+--  - Move everything to modules.
 
 main :: IO ()
 main =
@@ -46,10 +57,20 @@ main =
             { AWS.logger = logger
             }
     -- env <- AWS.newEnv AWS.discover
-    exampleS3 env
+    -- putStrLn "\n\nRunning example3"
+    -- exampleS3 env
+    putStrLn "\n\nRunning exampleDynamoDBListTables"
     exampleDynamoDBListTables env -- List tables. We expect Just [].
-    exampleDynamoDBCreateTable env -- Create a table. Called "foo"
-    exampleDynamoDBListTables env -- List tables. We expect Just ["foo"].
+    -- putStrLn "\n\nRunning exampleDynamoDBDeleteTable"
+    -- exampleDynamoDBDeleteTable env -- Delete the table.
+    -- putStrLn "\n\nRunning exampleDynamoDBCreateTable"
+    -- exampleDynamoDBCreateTable env -- Create a table. Called "foo"
+    -- putStrLn "\n\nRunning exampleDynamoDBListTables"
+    -- exampleDynamoDBListTables env -- List tables. We expect Just ["foo"].
+    -- putStrLn "\n\nRunning exampleDynamoDBDeleteTable"
+    -- exampleDynamoDBDeleteTable env -- Delete the table.
+    -- putStrLn "\n\nRunning exampleDynamoDBListTables"
+    -- exampleDynamoDBListTables env -- List tables. We expect Just [].
 
 -- Library functions
 
@@ -70,7 +91,56 @@ exampleDynamoDBCreateTable env = do
   respCreate <-
     AWS.runResourceT $
       AWS.send env $
-        newCreateTable "MyTable" (newKeySchemaElement "Id" KeyType_HASH :| [])
-          & createTable_attributeDefinitions .~ [newAttributeDefinition "Id" ScalarAttributeType_S]
+        newCreateTable
+          -- Table name
+          "MyTable"
+          -- Create Key Schema
+          (newKeySchemaElement "PrimaryKey" KeyType_HASH :| [newKeySchemaElement "SortKey" KeyType_RANGE])
+          -- Create Attribute Definitions
+          & createTable_attributeDefinitions
+            .~ [ newAttributeDefinition "PrimaryKey" ScalarAttributeType_S,
+                 newAttributeDefinition "SortKey" ScalarAttributeType_S
+               ]
+          -- Set Billing Mode
           & createTable_billingMode ?~ BillingMode_PAY_PER_REQUEST
   print $ respCreate ^. createTableResponse_tableDescription
+
+exampleDynamoDBDeleteTable :: AWS.Env -> IO ()
+exampleDynamoDBDeleteTable env = do
+  respCreate <-
+    AWS.runResourceT $
+      AWS.send env $
+        newDeleteTable "MyTable"
+  print $ respCreate ^. deleteTableResponse_tableDescription
+
+exampleDynamoDBPutItem ::
+  AWS.Env ->
+  -- TODO: Alias these parameter types
+  Text -> -- tableName
+  Text -> -- keyName
+  Text -> -- keyValue
+  Text -> -- attributeName
+  Text -> -- attributeValue
+  IO ()
+exampleDynamoDBPutItem env tableName keyName keyValue attributeName attributeValue = do
+  let item =
+        -- Hash Map of the data to be put
+        HM.fromList
+          [ (keyName, S keyValue),
+            (attributeName, S attributeValue)
+          ]
+  respCreate <-
+    AWS.runResourceT $
+      AWS.send env $
+        newPutItem tableName & putItem_item .~ item
+  print $ respCreate ^. putItemResponse_attributes
+
+exampleDynamoDBDescribeTable :: AWS.Env -> IO ()
+exampleDynamoDBDescribeTable env = do
+  respCreate <-
+    AWS.runResourceT $
+      AWS.send env $
+        newDescribeTable "MyTable"
+  print $ respCreate ^. describeTableResponse_table
+
+-- TODO: Querying data
